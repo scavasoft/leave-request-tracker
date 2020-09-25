ed7943fd4e9b4c55cb4cacbbd620e5ff0d694e6b
Vladislav <vlladislav45@gmail.com>
2020-09-25 16:32:42 +0300

    * Init react and redux

---
+++ frontend/src/initializationLayer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class InitializationLayer extends React.Component {
+    render() {
+        return(
+            <div>test</div>
+        );
+    }
+}
+
+export default (InitializationLayer);
